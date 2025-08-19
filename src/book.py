703a8f037e66ac77e7c269fd37780744ffb6257f
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-08-19 03:00:16 +0300

    refactor: Overall structure for book class changed.

--- src/book.py
+++ src/book.py
@@ -1,36 +1,74 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from datetime import datetime
+import re
 
-
-
+@dataclass(eq=True, frozen=True)
 class Book:
-    def __init__(self, isbn: str, title: str, author: str):
-        """Represents all information for a book."""
+    isbn: str
+    title: str
+    author: str
+    year: int
+    genre: Optional[str] = None
+    is_borrowed = bool = field(default=False, compare=False)
+    borrower_id = Optional[str] = field(default=None, compare=False)
+    borrowed_at = Optional[datetime] = field(default=None, compare=False)
+
+    def __post_init__(self, isbn: str):
+        """
+        Controlling if any of the important book information like isbn, title and author is empty,
+        or does not fit with the type of data requested.
+        """
+        self._isbn = isbn
+
+        regex = "^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\\d-]+$"
+
+        # Compiling Regex
+        compiled = re.compile(regex)
+
+        if (self._isbn == None):
+            raise ValueError
+        
+        # Return ISBN if mathces Regex format
+        if (re.search(compiled, self._isbn)):
+            return isbn
+        
+        else:
+            return f"{self._isbn} is not valid."
+        
+
+    
+    def _clean_isbn(self, isbn_string: str) -> str:
+        """Cleaning unnecessary characters from ISBN"""
+        self._isbn = isbn_string
+
+        self._isbn.strip()
+        self._isbn.split("-")
+        self._isbn.split(" ")
+
+
+
+
+
+        
+
+    def validate_isbn(self, isbn: str) -> bool:
+        pass
+
+    
+    def _validate_isbn10(self, isbn: str) -> str:
         self.isbn = isbn
-        self.title = title
-        self.author = author
-        self.is_borrowed = False
-
-    def __str__(self):
-        return f"'{self.title}' by {self.author} -> ISBN: {self.isbn}"
-    
-    def __repr__(self):
-        return f"Book(title={self.title!r}, author={self.author!r})"
-
-    def display_book_info(self) -> str:
-        print(f"Book ISBN: {self.isbn}\nBook title: {self.title}\n")
-
-    def borrow_book(self):
-        if not self.is_borrowed:
-            is_borrowed = True
-        else:
-            raise ValueError(f"{self.title} is already borrowed. Please search for another book to borrow.")
         
-    def return_book(self):
-        if self.is_borrowed:
-            is_borrowed = False
-        else:
-            raise ValueError(f"{self.title} was not borrowed.")
-        
+        self.isbn.split("-")
+        self.isbn_list = list(self.isbn)
+        for digit in self.isbn_list:
+            pass
+            
 
-    
+    def _validate_isbn13(self, isbn: str) -> str:
+        self.isbn = isbn
 
-    
+        self.isbn.split("-")
+        self.isbn_list = list(self.isbn)
+        for digit in self.isbn_list:
+            pass
