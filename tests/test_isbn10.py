09c2cec70853581399ac7f58a68f766f7b33e659
Muhammed Musab Kaya <032180115@ogr.uludag.edu.tr>
2025-08-31 22:09:02 +0300

    test: Test scripts updated.

--- tests/test_isbn10.py
+++ tests/test_isbn10.py
@@ -1,17 +1,42 @@
-def validate_isbn10(isbn: str) -> str:
-        isbn = isbn
+# def validate_isbn10(isbn: str) -> str:
+#         isbn = isbn
         
-        isbn_list = list(isbn)
-        multiplier = 10
-        sum = 0
-        for digit in isbn_list:
-            product = int(digit) * multiplier
-            multiplier -= 1
-            sum += product
+#         isbn_list = list(isbn)
+#         multiplier = 10
+#         sum = 0
+#         for digit in isbn_list:
+#             product = int(digit) * multiplier
+#             multiplier -= 1
+#             sum += product
 
-        if sum % 11 == 0:
-            return f"ISBN-10 '{isbn}' is valid."
-        else:
-            return f"ISBN-10 '{isbn}' is not valid!"
+#         if sum % 11 == 0:
+#             return f"ISBN-10 '{isbn}' is valid."
+#         else:
+#             return f"ISBN-10 '{isbn}' is not valid!"
         
-print(validate_isbn10("0306406153"))
+# print(validate_isbn10("0306406153"))
+
+
+def validate_isbn10(isbn: str) -> bool:
+
+        if len(isbn) != 10:
+            return False
+
+        # Checksum calculation
+        total = 0
+        for digit, char in enumerate(isbn):
+            if digit == 9 and char == "X": # Controling if last character is 'X'
+                value = 10
+            elif char.isdigit():
+                value = int(char)
+            else:
+                return False
+
+            weight = 10 - digit
+            total += value * weight
+
+
+        # Mod 11 Control: Is total value divisible by 11
+        return total % 11 == 0
+
+print(validate_isbn10("0306406152"))
--- tests/test_isbn13.py
+++ tests/test_isbn13.py
@@ -1,24 +1,33 @@
-def validate_isbn13(isbn: str) -> str:
-    isbn = isbn
+# def validate_isbn13(isbn: str) -> str:
+#     isbn = isbn
 
-    isbn_list = list(isbn)
-    sum = 0
-    multiply_one = isbn_list[0:13:2]
-    multiply_three = isbn_list[1:12:2]
+#     isbn_list = list(isbn)
+#     sum = 0
+#     multiply_one = isbn_list[0:13:2]
+#     multiply_three = isbn_list[1:12:2]
 
-    for digit_one in multiply_one:
-        result_one = int(digit_one) * 1
-        sum += result_one
+#     for digit_one in multiply_one:
+#         result_one = int(digit_one) * 1
+#         sum += result_one
 
-    for digit_three in multiply_three:
-        result_three = int(digit_three) * 3
-        sum += result_three
+#     for digit_three in multiply_three:
+#         result_three = int(digit_three) * 3
+#         sum += result_three
 
-    if sum % 10 == 0:
-        return f"ISBN-13 '{isbn}' is valid."
-    else:
-        return f"ISBN-13 '{isbn}' is not valid!"
+#     if sum % 10 == 0:
+#         return f"ISBN-13 '{isbn}' is valid."
+#     else:
+#         return f"ISBN-13 '{isbn}' is not valid!"
     
-print(validate_isbn13("9780306406157"))
+# print(validate_isbn13("9780306406157"))
 
+
+
+
+def validate_isbn13(isbn: str) -> bool:
         
+        total = sum(int(digit) * (3 if num % 2 == 1 else 1) for num, digit in enumerate(isbn))
+        
+        return total % 10 == 0
+
+print(validate_isbn13("9780136019701"))
