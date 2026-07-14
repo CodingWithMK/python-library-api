3cb58e17dc5ed121dab9e7f1bad7a51c87131433
CodingWithMK <032180115@ogr.uludag.edu.tr>
2026-07-15 01:37:55 +0200

    feat(library): add main library functions

--- src/library.py
+++ src/library.py
@@ -6,7 +6,33 @@ from typing import Optional, List
 @dataclass
 class Library:
     store_path: str
-    books: List
+    books: List[Book] = None
+
+    def add_book(self, book: Book):
+        if self.books is None:
+            self.books = []
+        self.books.append(book)
+
+    def remove_book(self, book: Book):
+        if self.books is not None:
+            self.books.remove(book)
+
+    def list_books(self):
+        if self.books is not None:
+            self.books
+
+    def find_book(self):
+        pass
+
+    def load_books(self):
+        if self.books is not None:
+            for book in self.books:
+                print(book, "\n")
+
+    def save_books(self):
+        pass
+
+
 
 
     
